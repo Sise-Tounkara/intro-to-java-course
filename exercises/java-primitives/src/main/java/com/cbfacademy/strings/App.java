085ac3abf0e91f5c7fa8377f178595f5936a6c37
Sise Tounkara <ceesaytunkara90@gmail.com>
2023-11-11 19:15:53 +0000

    Complete java primitives exercise

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -6,17 +6,21 @@ public class App {
     }
 
     public static String concatenate(String word1, String word2, String word3) {
-        // TODO: Write code that concatenates the input parameters and returns the result
-        throw new RuntimeException("Not implemented");
+        // TODO: Write code that concatenates the two input parameters and retuns the result.
+
+        return word1.concat(word2).concat(word3);
     }
 
     public static Boolean areEqual(String word1, String word2) {
-        // TODO: Write code to determine whether the input parameters are equal strings
-        throw new RuntimeException("Not implemented");
+        // TODO: Write code to determine whether the input parameters are equal strings.
+
+        return word1.equals(word2);
     }
 
     public static String format(String item, int quantity, double price) {
-        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
-        throw new RuntimeException("Not implemented");
+        //TODO: Write code to return a string formatted as follows: Item: [item]. Price: [price]. Quantity: [quantity]. The date should be formatted as DD/MM/YYYY.
+
+        return  String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
+
     }
 }
